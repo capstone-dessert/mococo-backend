1d7b5c759b88166b2d236978ef8702198e33a3cd
geonu <kgo000926@gmail.com>
2024-05-05 19:32:26 +0900

    feat: 전역 예외 처리

---
+++ src/main/java/capstone/dessert/mococobackend/exception/ApplicationException.java
@@ -0,0 +1,25 @@
+package capstone.dessert.mococobackend.exception;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public abstract class ApplicationException extends RuntimeException {
+    private final Map<String, String> validation = new HashMap<>();
+
+    protected ApplicationException(String message) {
+        super(message);
+    }
+
+    protected ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract int getStatusCode();
+
+    public void addValidation(String fieldName, String message) {
+        this.validation.put(fieldName, message);
+    }
+}
