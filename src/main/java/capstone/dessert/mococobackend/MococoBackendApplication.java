c31016eafe1755ccfa68cc0a8b03e7cd322d2dfe
geonu <kgo000926@gmail.com>
2024-05-05 16:59:04 +0900

    init project

---
+++ src/main/java/capstone/dessert/mococobackend/MococoBackendApplication.java
@@ -0,0 +1,13 @@
+package capstone.dessert.mococobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MococoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MococoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/capstone/dessert/mococobackend/MococoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.dessert.mococobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MococoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
