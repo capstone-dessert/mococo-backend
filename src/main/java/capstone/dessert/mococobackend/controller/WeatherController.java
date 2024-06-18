e093f66f5daa225c901235d0b5b81cfdab7e4155
geonu <kgo000926@gmail.com>
2024-06-18 17:10:37 +0900

    fix: 날씨 컨트롤러 get -> post

--- src/main/java/capstone/dessert/mococobackend/controller/WeatherController.java
+++ src/main/java/capstone/dessert/mococobackend/controller/WeatherController.java
@@ -5,10 +5,7 @@ import capstone.dessert.mococobackend.request.WeatherGeoRequest;
 import capstone.dessert.mococobackend.response.WeatherResponse;
 import capstone.dessert.mococobackend.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,12 +14,12 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
-    @GetMapping(path = "/address", consumes = "application/json", produces = "application/json")
+    @PostMapping(path = "/address", consumes = "application/json", produces = "application/json")
     public WeatherResponse getWeatherByDistrict(@RequestBody WeatherAddressRequest weatherRequest) {
         return weatherService.searchWeatherAddress(weatherRequest);
     }
 
-    @GetMapping(path = "/geo", consumes = "application/json", produces = "application/json")
+    @PostMapping(path = "/geo", consumes = "application/json", produces = "application/json")
     public WeatherResponse getWeatherByGeo(@RequestBody WeatherGeoRequest weatherRequest) {
         return weatherService.searchWeatherByGeo(weatherRequest);
     }
